40a11d76f2866bb9451843cfea9a8ef31f911278
drawrs <rizal.hilman67@gmail.com>
2024-05-27 07:29:12 +0700

    Initial Commit

---
+++ PhotosPicker PhotoUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotosPicker PhotoUI
+//
+//  Created by Rizal Hilman on 27/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotosPicker PhotoUI/PhotosPicker_PhotoUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosPicker_PhotoUIApp.swift
+//  PhotosPicker PhotoUI
+//
+//  Created by Rizal Hilman on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotosPicker_PhotoUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
